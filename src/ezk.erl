bc1649780544181e702d144a755e3f94fc9fdcfc
Marco <marco@gauss.gi.net>
2011-03-24 15:59:02 +0100

    help command added

--- src/ezk.erl
+++ src/ezk.erl
@@ -9,7 +9,44 @@
 %macros
 -export([delete_all/1]).
 %infos
--export([info_get_iterations/0]).
+-export([info_get_iterations/0, help/0]).
+%Stop commands (forcing Client to choose a new random Server from List)
+-export([die/0, die/1]).
+
+
+
+help() ->
+    io:format("-----------------------------------------------------~n"),
+    io:format("| The Commands this Client knows about:             |~n"),
+    io:format("|---------------------------------------------------|~n"),
+    io:format("| ezk:create/2     : Path, Data                     |~n"),
+    io:format("| ezk:create/3     : Path, Data, Typ                |~n"),
+    io:format("| ezk:create/4     : Path, Data, Typ, [Acl]         |~n"),
+    io:format("| ezk:delete/1     : Path                           |~n"),
+    io:format("| ezk:delete_all/1 : Path                           |~n"),
+    io:format("| ezk:get/1        : Path                           |~n"),
+    io:format("| ezk:get/3        : Path, WatchOwner, Watchmessage |~n"),
+    io:format("| ezk:get_acl/1    : Path                           |~n"),
+    io:format("| ezk:set/2        : Path, Data                     |~n"),
+    io:format("| ezk:set_acl/2    : Path, [Acl]                    |~n"),
+    io:format("| ezk:ls/1         : Path                           |~n"),
+    io:format("| ezk:ls/3         : Path, WatchOwner, Watchmessage |~n"),
+    io:format("| ezk:ls2/1        : Path                           |~n"),
+    io:format("| ezk:ls2/3        : Path, WatchOwner, Watchmessage |~n"),
+    io:format("| ezk:die/0                                         |~n"),
+    io:format("| ezk:die/1        : Reason                         |~n"),
+    io:format("| ezk:info_get_iterations/0                         |~n"),
+    io:format("|---------------------------------------------------|~n"),
+    io:format("| In Progress:                                      |~n"),
+    io:format("| ezk:auth/2       : Scheme, Id                     |~n"),
+    io:format("|---------------------------------------------------|~n"),
+    io:format("| Datatypes:                                        |~n"),
+    io:format("| Acl = {Scheme,Id, [Permission]}                   |~n"),
+    io:format("| Path = Scheme = Id = Reason = String              |~n"),  
+    io:format("| Permission = r | w | c | d | a                    |~n"),
+    io:format("| WatchOwner = PId           WatchMessage = String  |~n"),
+    io:format("| Data = All Things          Typ = e | s | es       |~n"),
+    io:format("|---------------------------------------------------|~n").
 
 
 %%--------------------------- Zookeeper Functions ---------------------
@@ -94,3 +131,10 @@ ls2(Path, WatchOwner, WatchMessage) ->
 %% Reply = Iteration = Int.
 info_get_iterations() ->
     ezk_connection:info_get_iterations().
+
+die() ->
+    ezk_connection:die("No offence").
+
+die(Reason) ->
+    ezk_connection:die(Reason).
+    

deb270ced628f0b56e19cb26087e53fb0f634c80
Marco <marco@gauss.(none)>
2011-04-28 12:06:24 +0200

    If connections are ended there is no longer an error logged.
    fixes #1856

--- src/ezk_connection.erl
+++ src/ezk_connection.erl
@@ -130,7 +130,7 @@ handle_call({watchcommand, {Command, CommandW, Path, {WType, WO, WM}}}, From, St
 %% Handles orders to die by dying
 handle_call({die, Reason}, _From, State) ->
     ?LOG(3," Connection: exiting myself"),
-    {stop, Reason, ok, State};
+    {stop, {shutdown, Reason}, ok, State};
 %% Handles auth requests
 handle_call({addauth, Scheme, Auth}, From, State) ->
     OutstandingAuths = State#cstate.outstanding_auths,

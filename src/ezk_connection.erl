af5935c641be281f86e8fbd06869825662042c22
Marco <marco@gauss.(none)>
2011-05-05 14:43:02 +0200

    optimisation of tabing

--- src/ezk_connection.erl
+++ src/ezk_connection.erl
@@ -98,55 +98,35 @@ addauth(ConnectionPId, Scheme, Auth) ->
 
 %% Creates a new ZK_Node
 %% Reply = Path where Path = String
-create(ConnectionPId, Path, Data) ->
-     
-		  gen_server:call(ConnectionPId, {command, {create, Path, 
-							    Data, [], [undef]}}).
-n_create(ConnectionPId, Path, Data, Receiver, Tag) ->
-     
-		  gen_server:cast(ConnectionPId, {nbcommand, {create, Path,
-							      Data, [], [undef]}, 
-				   Receiver, Tag}).
+create(ConnectionPId, Path, Data) ->     
+    gen_server:call(ConnectionPId, {command, {create, Path, Data, [], [undef]}}).
+n_create(ConnectionPId, Path, Data, Receiver, Tag) ->     
+    gen_server:cast(ConnectionPId, {nbcommand, {create, Path, Data, [], [undef]}, 
+				    Receiver, Tag}).
 %% Typ = e | s | es (stands for etheremal, sequenzed or both)
-create(ConnectionPId, Path, Data, Typ) ->
-     
-		  gen_server:call(ConnectionPId, {command, {create, Path,
-							    Data, Typ, [undef]}}).
-n_create(ConnectionPId, Path, Data, Typ, Receiver, Tag) ->
-     
-		  gen_server:cast(ConnectionPId, {nbcommand, {create, Path,
-							      Data, Typ, [undef]}, 
-				   Receiver, Tag}).
+create(ConnectionPId, Path, Data, Typ) ->     
+    gen_server:call(ConnectionPId, {command, {create, Path, Data, Typ, [undef]}}).
+n_create(ConnectionPId, Path, Data, Typ, Receiver, Tag) ->     
+    gen_server:cast(ConnectionPId, {nbcommand, {create, Path, Data, Typ, [undef]}, 
+				    Receiver, Tag}).
 
 %% Acls = [Acl] where Acl = {Permissions, Scheme, Id} 
 %% with Scheme and Id = String
 %% and Permission = [Per] | String 
 %% where Per = r | w | c | d | a
-create(ConnectionPId, Path, Data, Typ, Acls)  ->
-     
-		  gen_server:call(ConnectionPId, {command, {create, Path,
-							    Data, Typ, Acls}}).
-n_create(ConnectionPId, Path, Data, Typ, Acls, Receiver, Tag)  ->
-     
-		  gen_server:cast(ConnectionPId, {nbcommand, {create, Path,
-							      Data, Typ, Acls}, 
-				   Receiver, Tag}).
-
-ensure_path(ConnectionPId, Path) ->
-     
-		  macro_ensure_path(ConnectionPId, Path).
+create(ConnectionPId, Path, Data, Typ, Acls)  ->     
+    gen_server:call(ConnectionPId, {command, {create, Path, Data, Typ, Acls}}).
+n_create(ConnectionPId, Path, Data, Typ, Acls, Receiver, Tag)  ->     
+    gen_server:cast(ConnectionPId, {nbcommand, {create, Path, Data, Typ, Acls}, 
+				    Receiver, Tag}).
 
 %% Deletes a ZK_Node
 %% Only working if Node has no children.
 %% Reply = Path where Path = String
-delete(ConnectionPId, Path) ->
-     
-		  gen_server:call(ConnectionPId, {command, {delete,  Path,
-							    []}}).
-n_delete(ConnectionPId, Path, Receiver, Tag) ->
-     
-		  gen_server:cast(ConnectionPId, {nbcommand, {delete,  Path,
-							      []}, Receiver, Tag}).
+delete(ConnectionPId, Path) ->     
+    gen_server:call(ConnectionPId, {command, {delete,  Path, []}}).
+n_delete(ConnectionPId, Path, Receiver, Tag) ->     
+    gen_server:cast(ConnectionPId, {nbcommand, {delete,  Path, []}, Receiver, Tag}).
 
 %% Deletes a ZK_Node and all his childs.
 %% Reply = Path where Path = String
@@ -154,114 +134,10 @@ n_delete(ConnectionPId, Path, Receiver, Tag) ->
 %% or gets other errors the function tries the
 %% other nodes befor giving the error back, so a 
 %% maximum number of nodes is deleted.
-delete_all(ConnectionPId, Path) ->
-     
-		  macro_delete_all_childs(ConnectionPId, Path).    
-
-%% Reply = {Data, Parameters} where Data = The Data stored in the Node
-%% and Parameters = {getdata, Czxid, Mzxid, Pzxid, Ctime, Mtime, Dataversion,
-%%                   Datalength, Number_children, Cversion, Aclversion, Ephe_owner}
-get(ConnectionPId, Path) ->
-     
-		  gen_server:call(ConnectionPId, {command, {get, Path}}).
-n_get(ConnectionPId, Path, Receiver, Tag) ->
-     
-		  gen_server:cast(ConnectionPId, {command, {get, Path},
-						  Receiver, Tag}).
-%% Like the one above but sets a datawatch to Path.
-%% If watch is triggered a Message M is send to the PId WatchOwner
-%% M = {WatchMessage, {Path, Type, SyncCon}
-%% with Type = child
-get(ConnectionPId, Path, WatchOwner, WatchMessage) ->
-     
-		  gen_server:call(ConnectionPId, {watchcommand, {get, getw, 
-								 Path,
-								 {data, WatchOwner,
-								  WatchMessage}}}).
-
-%% Returns the actual Acls of a Node
-%% Reply = {[ACL],Parameters} with ACl and Parameters like above
-get_acl(ConnectionPId, Path) ->
-     
-		  gen_server:call(ConnectionPId, {command, {get_acl, 
-							    Path}}).
-n_get_acl(ConnectionPId, Path, Receiver, Tag) ->
-     
-		  gen_server:cast(ConnectionPId, {command, {get_acl, Path},
-						  Receiver, Tag}).
-
-%% Sets new Data in a Node. Old ones are lost.
-%% Dataformat is Binary.
-%% Reply = Parameters with Data like at get
-set(ConnectionPId, Path, Data) ->
-     
-		  gen_server:call(ConnectionPId, {command, {set, Path,
-							    Data}}).
-n_set(ConnectionPId, Path, Data, Receiver, Tag) ->
-     
-		  gen_server:cast(ConnectionPId, {command, {set, Path,
-							    Data}, Receiver, Tag}).
-
-%% Sets new Acls in a Node. Old ones are lost.
-%% ACL like above.
-%% Reply = Parameters with Data like at get
-set_acl(ConnectionPId, Path, Acls) ->
-     
-		  gen_server:call(ConnectionPId, {command, {set_acl, Path,
-							    Acls}}).
-n_set_acl(ConnectionPId, Path, Acls, Receiver, Tag) ->
-     
-		  gen_server:cast(ConnectionPId, {command, {set_acl, Path,
-							    Acls}, Receiver, Tag}).
-
-%% Lists all Children of a Node. Paths are given as Binarys!
-%% Reply = [ChildName] where ChildName = <<"Name">>
-ls(ConnectionPId, Path) ->
-     
-		  gen_server:call(ConnectionPId, {command, {ls, Path}}).
-n_ls(ConnectionPId, Path, Receiver, Tag) ->
-     
-		  gen_server:cast(ConnectionPId, {nbcommand, {ls, Path},
-						  Receiver, Tag}).
-%% like above, but a Childwatch is set to the Node. 
-%% Same Reaktion like at get with watch but Type = child
-ls(ConnectionPId, Path, WatchOwner, WatchMessage) ->
-    ?LOG(3,"Connection: Send lsw"),
-     
-		  gen_server:call(ConnectionPId, {watchcommand, {ls, lsw,  
-								 Path,
-								 {child, WatchOwner,
-								  WatchMessage}}}).
-
-%% Lists all Children of a Node. Paths are given as Binarys!
-%% Reply = {[ChildName],Parameters} with Parameters and ChildName like above.
-ls2(ConnectionPId, Path) ->
-     
-		  gen_server:call(ConnectionPId, {command, {ls2, Path}}).
-n_ls2(ConnectionPId, Path, Receiver, Tag) ->
-     
-		  gen_server:cast(ConnectionPId, {command, {ls2, Path},
-						  Receiver, Tag}).
-%% like above, but a Childwatch is set to the Node. 
-%% Same Reaktion like at get with watch but Type = child
-ls2(ConnectionPId, Path, WatchOwner, WatchMessage) ->
-     
-		  gen_server:call(ConnectionPId, {watchcommand, {ls2, ls2w,
-								 Path
-								 ,{child, WatchOwner,
-								   WatchMessage}}}).
-
-%% Returns the Actual Transaction Id of the Client.
-%% Reply = Iteration = Int.
-info_get_iterations(ConnectionPId) ->
-    gen_server:call(ConnectionPId, {info, get_iterations}).
-
-
-%% A Macro which deletes a Node and all his Childs.
 %% a) List children of Node. If he has none everything is all right.
 %% b) If he has some: kill them and their Children rekursively.
 %% c) Kill the Node with delete    
-macro_delete_all_childs(ConnectionPId, Path) ->
+delete_all(ConnectionPId, Path) ->
     ?LOG(3, "Delete All: Trying to Erase ~s",[Path]),
     Childs = ls(ConnectionPId, Path),
     case Childs of
@@ -286,11 +162,81 @@ macro_delete_all_childs(ConnectionPId, Path) ->
             delete(ConnectionPId, Path);
 	{error, Message} ->
 	    {error, Message}
-    end.
+    end.       
+
+%% Reply = {Data, Parameters} where Data = The Data stored in the Node
+%% and Parameters = {getdata, Czxid, Mzxid, Pzxid, Ctime, Mtime, Dataversion,
+%%                   Datalength, Number_children, Cversion, Aclversion, Ephe_owner}
+get(ConnectionPId, Path) ->     
+		  gen_server:call(ConnectionPId, {command, {get, Path}}).
+n_get(ConnectionPId, Path, Receiver, Tag) ->     
+		  gen_server:cast(ConnectionPId, {command, {get, Path}, Receiver, Tag}).
+%% Like the one above but sets a datawatch to Path.
+%% If watch is triggered a Message M is send to the PId WatchOwner
+%% M = {WatchMessage, {Path, Type, SyncCon}
+%% with Type = child
+get(ConnectionPId, Path, WatchOwner, WatchMessage) ->     
+    gen_server:call(ConnectionPId, {watchcommand, {get, getw, Path, {data, WatchOwner,
+								     WatchMessage}}}).
+
+%% Returns the actual Acls of a Node
+%% Reply = {[ACL],Parameters} with ACl and Parameters like above
+get_acl(ConnectionPId, Path) ->     
+    gen_server:call(ConnectionPId, {command, {get_acl, Path}}).
+n_get_acl(ConnectionPId, Path, Receiver, Tag) ->     
+    gen_server:cast(ConnectionPId, {command, {get_acl, Path}, Receiver, Tag}).
+
+%% Sets new Data in a Node. Old ones are lost.
+%% Dataformat is Binary.
+%% Reply = Parameters with Data like at get
+set(ConnectionPId, Path, Data) ->     
+    gen_server:call(ConnectionPId, {command, {set, Path, Data}}).
+n_set(ConnectionPId, Path, Data, Receiver, Tag) ->     
+    gen_server:cast(ConnectionPId, {command, {set, Path, Data}, Receiver, Tag}).
+
+%% Sets new Acls in a Node. Old ones are lost.
+%% ACL like above.
+%% Reply = Parameters with Data like at get
+set_acl(ConnectionPId, Path, Acls) ->     
+    gen_server:call(ConnectionPId, {command, {set_acl, Path, Acls}}).
+n_set_acl(ConnectionPId, Path, Acls, Receiver, Tag) ->     
+    gen_server:cast(ConnectionPId, {command, {set_acl, Path, Acls}, Receiver, Tag}).
+
+%% Lists all Children of a Node. Paths are given as Binarys!
+%% Reply = [ChildName] where ChildName = <<"Name">>
+ls(ConnectionPId, Path) ->     
+    gen_server:call(ConnectionPId, {command, {ls, Path}}).
+n_ls(ConnectionPId, Path, Receiver, Tag) ->     
+    gen_server:cast(ConnectionPId, {nbcommand, {ls, Path}, Receiver, Tag}).
+%% like above, but a Childwatch is set to the Node. 
+%% Same Reaktion like at get with watch but Type = child
+ls(ConnectionPId, Path, WatchOwner, WatchMessage) ->
+    ?LOG(3,"Connection: Send lsw"),     
+    gen_server:call(ConnectionPId, {watchcommand, {ls, lsw, Path, {child, WatchOwner,
+								   WatchMessage}}}).
+
+%% Lists all Children of a Node. Paths are given as Binarys!
+%% Reply = {[ChildName],Parameters} with Parameters and ChildName like above.
+ls2(ConnectionPId, Path) ->
+     
+		  gen_server:call(ConnectionPId, {command, {ls2, Path}}).
+n_ls2(ConnectionPId, Path, Receiver, Tag) ->     
+		  gen_server:cast(ConnectionPId, {command, {ls2, Path},
+						  Receiver, Tag}).
+%% like above, but a Childwatch is set to the Node. 
+%% Same Reaktion like at get with watch but Type = child
+ls2(ConnectionPId, Path, WatchOwner, WatchMessage) ->
+    gen_server:call(ConnectionPId, {watchcommand, {ls2, ls2w,Path ,{child, WatchOwner,
+								    WatchMessage}}}).
+
+%% Returns the Actual Transaction Id of the Client.
+%% Reply = Iteration = Int.
+info_get_iterations(ConnectionPId) ->
+    gen_server:call(ConnectionPId, {info, get_iterations}).
 
 %% Gets a path and looks if the corresponding node exists. If
 %% not it is created (along with the whole path).
-macro_ensure_path(ConnectionPId, Path) ->
+ensure_path(ConnectionPId, Path) ->
     FolderList = string:tokens(Path, "/"),
     PrefixPaths = get_prefix_paths(FolderList),
     lists:map(fun(Folder) -> ensure_folder(ConnectionPId, Folder) end, PrefixPaths),
